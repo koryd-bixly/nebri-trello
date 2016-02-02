6dc0c937e634a0b64f33830ff66cd4d4f09a471e
kory donati <koryd@bixly.com>
2016-02-02 09:20:23 -0800

    added account permission

--- scripts/trello_poll_watched_boards.py
+++ scripts/trello_poll_watched_boards.py
@@ -4,7 +4,7 @@ from trello import TrelloClient
 
 class trello_poll_watched_boards(NebriOS):
 
-    schedule = '0 0 * * *'
+    schedule = '0 */2 * * *'
 
     def action(self):
         # TODO add this to some general login script
@@ -18,15 +18,39 @@ class trello_poll_watched_boards(NebriOS):
         # watched_boards = Boards.filter()
         watched_boards = client.list_boards()  # TEMP
 
+        # setup query_params
+        params = dict(
+            checklists='all'
+        )
+
         for board in watched_boards:
             # due in cards does not seem to be working properly...
             # cards =
+            cards = client.fetch_json(
+                'boards/{}/cards/'.format(board.id),
+                query_params=params
+            )
             for card in board.open_cards():
-                card.fetch() # extra api call...
-                duedate = card.due_date()
+                due = card.get('due', None)
+                # convert string due to datetime
+                try:
+                    duedate = datetime.strptime(due, '%Y-%m-%dT%H:%M:%S.%fZ')
+                except Exception as e:
+                    # if unable to process, then go to next card
+                    logging.info(
+                        'could not parse datetime on card: ' + card.get('id')
+                    )
+                    continue
+
                 now = datetime.now()
                 oneday = now + timedelta(days=1)
-                checklists = card.checklists
+                checklists = card.get('checklists')
+                for checklist in checklists:
+                    finished = all(
+                        item.get('state') == 'complete'
+                        for item in checklist.get('checkItems')
+                    )
+
                 if duedate and checklists and now <= duedate <= oneday:
                     # checks if all items in checklist are finished
                     finished = all([item.get('checked', False) for item in
