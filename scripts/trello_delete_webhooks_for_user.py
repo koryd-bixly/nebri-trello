8434bbecf79b76de85b0546be087b6e75c51a1ae
Brie Marvin <briem@bixly.com>
2016-02-05 09:17:35 -0800

    added func for delete webhooks

--- scripts/trello_delete_webhooks_for_user.py
+++ scripts/trello_delete_webhooks_for_user.py
@@ -1,9 +1,4 @@
-import logging
-
-from trello import TrelloClient
-
-
-logging.basicConfig(filename='trello_webhook_module.log', level=logging.DEBUG)
+from trello_utils import delete_hooks
 
 
 class trello_delete_webhooks_for_user(NebriOS):
@@ -17,39 +12,7 @@ class trello_delete_webhooks_for_user(NebriOS):
         self.trello_delete_webhooks_for_user = "Ran"
         logging.debug('Doing trello_delete_webhooks_for_user action...')
 
-        client = self.get_client()
-
-        hooks = client.list_hooks()
-        
-        for hook in hooks:
-            logging.debug('Deleting Webhook %s' % hook.id)
-            client.fetch_json(
-                '/webhooks/%s' % hook.id,
-                http_method='DELETE'
-            )
-        
-    def get_client(self):
-        return TrelloClient(
-            api_key=shared.TRELLO_API_KEY,
-            api_secret=shared.TRELLO_API_SECRET,
-            token=self.trello_token
-        )
-    
-    def get_me(self):
-        client = self.get_client()
-        
-        return client.fetch_json('/members/me')
-
-    def verify_user(self):
-        verified = False
-        # Try authenticating with the credentials provided
-        # We should not get an authorization error
-        
-        try:
-            self.get_me()
-            verified = True
-        except Exception:
-            # We failed to authenticate with the provided credentials
-            pass
-        
-        return verified
+        if self.hook_id:
+            delete_hooks(self.last_actor, self.hook_id)
+        else:
+            delete_hooks(self.last_actor)
